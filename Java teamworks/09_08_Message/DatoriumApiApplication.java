6546cef7203475c66fcf0ab2d4ce79ec164177b4
Anu <159231883+anusalundi@users.noreply.github.com>
2024-08-09 15:48:41 +0300

    Update DatoriumApiApplication.java

--- Java teamworks/09_08_Message/DatoriumApiApplication.java
+++ Java teamworks/09_08_Message/DatoriumApiApplication.java
@@ -1,9 +1,14 @@
-package com.datorium.Datorium.API.Services;
+package com.datorium.Datorium.API;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
-import com.datorium.Datorium.API.DTOs.Message;
+@SpringBootApplication
+@CrossOrigin
+public class DatoriumApiApplication {
 
-public class MessageService {
-    public String add(Message message) {
-        return "Good luck learning programming, " + message.name + " :)";
-    }
+	public static void main(String[] args) {
+		System.out.println("asd");
+		SpringApplication.run(DatoriumApiApplication.class, args);
+	}
 }
