84b0c4d5d6164a72942cdd99d817aada7e5e02ae
Anu <159231883+anusalundi@users.noreply.github.com>
2024-08-21 19:59:31 +0300

    Update 21_08_SQL_PetClinic.sql

--- Java teamworks/21_08_SQL_PetClinic.sql
+++ Java teamworks/21_08_SQL_PetClinic.sql
@@ -1,4 +1,4 @@
 SELECT * FROM Pets WHERE Kind='Dog' AND Name GLOB '*An*';
-SELECT * FROM pets WHERE kind= 'Cat' OR kind= 'Rabbit';
+SELECT * FROM pets WHERE kind= 'Cat' OR kind='Rabbit';
 SELECT * FROM pets WHERE age BETWEEN 1 AND 5;
 SELECT City, COUNT(*) AS OwnerCount FROM owners GROUP BY City ORDER BY OwnerCount DESC LIMIT 5;
