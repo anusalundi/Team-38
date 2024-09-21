273866dd0573ff3c2f1ced29268a72a4c23f51cb
Anu <159231883+anusalundi@users.noreply.github.com>
2024-09-21 12:08:37 +0300

    Create 21-09_Random_Walk.py

---
+++ Python teamworks/21-09_Random_Walk.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+n = 100000  # number of steps
+
+x = np.zeros(n)
+
+for i in range(1, n):
+    step = random.choice([-1, 1])  
+    x[i] = x[i-1] + step  
+
+# Visualisation
+plt.figure(figsize=(10, 6)) 
+plt.plot(x, color='skyblue', lw=2, alpha=0.7) 
+plt.scatter(0, x[0], c='green', s=100, label='Start')  
+plt.scatter(n-1, x[-1], c='red', s=100, label='Finish')  
+
+plt.title(f'1D Random Walk with {n} steps', fontsize=16)
+plt.xlabel('Number of steps')
+plt.ylabel('Location')
+plt.grid(True)
+plt.legend()
+
+plt.show()
