557b69e5757d2f02434e92c0e14341b24d917884
Anu <159231883+anusalundi@users.noreply.github.com>
2024-09-25 20:26:22 +0300

    Create NonSpecialCount.java

---
+++ Java teamworks/Leetcode/NonSpecialCount.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int nonSpecialCount(int l, int r) {
+        int notSpecialCount = 0;
+        
+        for (int i = l; i <= r; i++) {
+            if (!isSpecial(i)) {
+                notSpecialCount++;  // if not special, then ++
+            }
+        }
+        
+        return notSpecialCount;
+    }
+    
+    private boolean isSpecial(int num) {
+        int divisorCount = 0;
+        
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                divisorCount++;  // dividor i
+                if (i != num / i && i != 1) {
+                    divisorCount++;  // dividor num / i
+                }
+            }
+        }
+        
+        return divisorCount == 2;
+    }
+}
